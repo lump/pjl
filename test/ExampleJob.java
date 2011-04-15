f02bc887d55b7e55858569a92abcf24eaf52441f
Troy Bowman <troy@lump.net>
2011-04-15 15:44:25 -0600

    Added an ExampleJob class and a TestPrinting testng test.

---
+++ test/ExampleJob.java
@@ -0,0 +1,150 @@
+import net.lump.print.jetdirect.SocketClient;
+import net.lump.print.jetdirect.pjl.commands.*;
+import net.lump.print.jetdirect.pjl.enums.LanguagePersonality;
+import net.lump.print.jetdirect.pjl.events.InputEvent;
+import net.lump.print.jetdirect.pjl.events.InputEventListener;
+import net.lump.print.jetdirect.pjl.ustatustypes.*;
+
+import static net.lump.print.PrintConstants.*;
+
+import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+/**
+ * This is an example of using this library to talk to a printer.
+ *
+ * @version $Id$
+ */
+public class ExampleJob {
+
+   public static void main(String[] args) {
+
+      // check for our expected hostname argument
+      if (args.length != 1) {
+         System.err.println("please provide a printer hostname or ip address to test with");
+         System.exit(1);
+      }
+
+      // resolve the hostname
+      InetAddress address = null;
+      try
+      {
+         address = Inet4Address.getByName(args[0]);
+      }
+      catch (UnknownHostException e)
+      {
+         System.err.println("please use a printer that exists");
+         System.exit(1);
+      }
+
+      // this is a job name we will use
+      String jobName = "TestJob-" + System.currentTimeMillis() + Math.random();
+      // this will hold the PJL commands we will issue
+      ArrayList<Command> commands = new ArrayList<Command>();
+
+      // this is a comment PJL command
+      commands.add(new Comment(" Testing 1 2 3 "));
+
+      // this listener will listen for unsolicited events and print them to stderr
+      // you could use these events to notify your application of printer status events
+      InputEventListener unsolicitedStatusListener = new InputEventListener()
+      {
+         public void inputEventOccurred(InputEvent event)
+         {
+            if (event.getSource() instanceof UnsolicitedStatusType)
+            {
+               UnsolicitedStatusType usd = (UnsolicitedStatusType)event.getSource();
+               System.err.printf(usd.toString() + "\n");
+            }
+         }
+      };
+
+      // create a PJL command that uses our listener above for Device events
+      UnsolicitedStatus printerStatus = new UnsolicitedStatus(new UnsolicitedDeviceType(UnsolicitedDeviceType.InputValue.ON));
+      printerStatus.addInputListener(unsolicitedStatusListener);
+      commands.add(printerStatus);
+
+      // if you want printer status every 5 seconds, use this.
+      // i've used this before as a keepalive to tell my application that the printer is still there
+      UnsolicitedStatus timedPrinterStatus = new UnsolicitedStatus(new UnsolicitedTimedType(5));
+      timedPrinterStatus.addInputListener(unsolicitedStatusListener);
+      commands.add(timedPrinterStatus);
+
+
+      // create a PJL job that will display "Troy's Test Job" in the printer's display
+      Job j = new Job(jobName, "Troy's Test Job");
+
+      // add a page listener to our PJL job
+      j.getPageUStatus().addInputListener(new InputEventListener()
+      {
+         public void inputEventOccurred(InputEvent event)
+         {
+            if (event.getSource() instanceof UnsolicitedPageType)
+            {
+               System.err.println("Printed page " + ((UnsolicitedPageType)event.getSource()).getPage());
+            }
+         }
+      });
+
+      // add a job listener to our PJL job
+      j.getJobUStatus().addInputListener(new InputEventListener()
+      {
+         public void inputEventOccurred(InputEvent event)
+         {
+            if (event.getSource() instanceof UnsolicitedJobType)
+            {
+               System.err.println(event.getSource().toString());
+            }
+         }
+      });
+
+      // add the job to our list
+      commands.add(j);
+
+      // wrap our Raw command (below) in a Postscript PJL personality
+      commands.add(new EnterLanguage(LanguagePersonality.POSTSCRIPT));
+
+      // this is a Raw command, it basically will BLORT out the string you provide to the socket
+      Raw raw = new Raw(
+         "%!PS" + CRLF
+         + "5 setlinewidth" + CRLF
+         + "100 100 moveto" + CRLF
+         + "0 300 rlineto" + CRLF
+         + "300 0 rlineto" + CRLF
+         + "0 -300 rlineto" + CRLF
+         + "closepath" + CRLF
+         + "stroke" + CRLF
+         + "185 240 moveto" + CRLF
+         + "/Helvetica findfont 20 scalefont setfont" + CRLF
+         + "(PostScript Job) show" + CRLF
+         + "showpage" + CRLF);
+
+      // add the raw to our list
+      commands.add(raw);
+      // add an EOJ PJL command
+      commands.add(new EndOfJob(jobName));
+
+
+      // our commands are ready to go now.  Let's
+      String output = null;
+      try {
+         SocketClient sc = SocketClient.getSocket(address);
+         output = sc.invokePjlCommands(commands);
+
+         // if there was any unexpected output from our job, it's probably a bad thing, as we weren't expecting it
+         // the thing that usually comes from this is postscript errors
+         if (output.length() > 0)
+         {
+            System.err.println("OUTPUT: " + output);
+         }
+      } catch (IOException e) {
+         System.err.println("IO Exception with printer " + address.getCanonicalHostName());
+         System.err.println(e.getMessage());
+         System.exit(1);
+      }
+
+   }
+}
